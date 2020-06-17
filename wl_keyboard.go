b19347e90a86eb18db2f66dcc5c320deba01a0a0
Kenny Levinsen <kl@kl.wtf>
2020-06-17 23:06:20 +0200

    Better seat object info

---
+++ wl_keyboard.go
@@ -0,0 +1,157 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+type WlKeyboardModifiers struct {
+	Depressed uint32
+	Latched   uint32
+	Locked    uint32
+	Group     uint32
+}
+
+type WlKeyboardRepeatInfo struct {
+	Rate  int32
+	Delay int32
+}
+
+type WlKeyboard struct {
+	Object         *WaylandObject
+	Seat           *WlSeat
+
+	EnteredSurface *WlSurface
+	Modifiers      WlKeyboardModifiers
+	RepeatInfo     WlKeyboardRepeatInfo
+	KeyEvents      int
+	KeysHeld       int
+}
+
+func (r *WlKeyboard) RandomInfo() string {
+	if r.EnteredSurface != nil {
+		return fmt.Sprintf(", keys held: %d, entered: %s", r.KeysHeld, r.EnteredSurface.Object)
+	} else {
+		return fmt.Sprintf(", keys held: %d", r.KeysHeld)
+	}
+}
+
+func (r *WlKeyboard) Destroy() error {
+	for idx := range r.Seat.Children {
+		if r.Seat.Children[idx].Data == r {
+			r.Seat.Children = append(r.Seat.Children[:idx], r.Seat.Children[idx+1:]...)
+			break
+		}
+	}
+	return nil
+}
+
+type WlKeyboardImpl struct {
+	client *Client
+}
+
+func RegisterWlKeyboard(client *Client) {
+	r := &WlKeyboardImpl{
+		client: client,
+	}
+	client.Impls["wl_keyboard"] = r
+}
+
+func (r *WlKeyboardImpl) Request(packet *WaylandPacket) error {
+	object := r.client.ObjectMap[packet.ObjectId]
+	_, ok := object.Data.(*WlKeyboard)
+	if !ok {
+		return errors.New("object is not a wl_keyboard")
+	}
+	switch packet.Opcode {
+	case 0: // release
+	}
+	return nil
+}
+
+func (r *WlKeyboardImpl) Event(packet *WaylandPacket) error {
+	object := r.client.ObjectMap[packet.ObjectId]
+	obj, ok := object.Data.(*WlKeyboard)
+	if !ok {
+		return errors.New("object is not a wl_keyboard")
+	}
+	var err error
+	switch packet.Opcode {
+	case 0: // keymap
+	case 1: // enter
+		_, err := packet.ReadUint32()
+		if err != nil {
+			return err
+		}
+		sid, err := packet.ReadUint32()
+		if err != nil {
+			return err
+		}
+		surface_obj := r.client.ObjectMap[sid]
+		if surface_obj == nil {
+			return errors.New("no such object")
+		}
+		surface, ok := surface_obj.Data.(*WlSurface)
+		if !ok {
+			return errors.New("object is not surface")
+		}
+		obj.EnteredSurface = surface
+	case 2: // leave
+		obj.EnteredSurface = nil
+	case 3: // key
+		_, err = packet.ReadUint32()
+		if err != nil {
+			return err
+		}
+		_, err = packet.ReadUint32()
+		if err != nil {
+			return err
+		}
+		_, err = packet.ReadUint32()
+		if err != nil {
+			return err
+		}
+		state, err := packet.ReadUint32()
+		if err != nil {
+			return err
+		}
+		switch state {
+		case 0:
+			obj.KeysHeld -= 1
+		case 1:
+			obj.KeysHeld += 1
+		}
+		obj.KeyEvents += 1
+	case 4: // modifiers
+		_, err := packet.ReadUint32()
+		if err != nil {
+			return err
+		}
+		obj.Modifiers.Depressed, err = packet.ReadUint32()
+		if err != nil {
+			return err
+		}
+		obj.Modifiers.Latched, err = packet.ReadUint32()
+		if err != nil {
+			return err
+		}
+		obj.Modifiers.Locked, err = packet.ReadUint32()
+		if err != nil {
+			return err
+		}
+		obj.Modifiers.Group, err = packet.ReadUint32()
+		if err != nil {
+			return err
+		}
+	case 5: // repeat_info
+		obj.RepeatInfo.Rate, err = packet.ReadInt32()
+		if err != nil {
+			return err
+		}
+		obj.RepeatInfo.Delay, err = packet.ReadInt32()
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
---
+++ wl_touch.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"errors"
+)
+
+type WlTouch struct {
+	Object         *WaylandObject
+	Seat           *WlSeat
+}
+
+func (r *WlTouch) Destroy() error {
+	for idx := range r.Seat.Children {
+		if r.Seat.Children[idx].Data == r {
+			r.Seat.Children = append(r.Seat.Children[:idx], r.Seat.Children[idx+1:]...)
+			break
+		}
+	}
+	return nil
+}
+
+type WlTouchImpl struct {
+	client *Client
+}
+
+func RegisterWlTouch(client *Client) {
+	r := &WlTouchImpl{
+		client: client,
+	}
+	client.Impls["wl_touch"] = r
+}
+
+func (r *WlTouchImpl) Request(packet *WaylandPacket) error {
+	object := r.client.ObjectMap[packet.ObjectId]
+	_, ok := object.Data.(*WlTouch)
+	if !ok {
+		return errors.New("object is not a wl_touch")
+	}
+	switch packet.Opcode {
+	case 0: // release
+	}
+	return nil
+}
+
+func (r *WlTouchImpl) Event(packet *WaylandPacket) error {
+	switch packet.Opcode {
+	case 0: // down
+	case 1: // up
+	case 2: // motion
+	case 3: // frame
+	case 4: // cancel
+	case 5: // shape
+	case 6: // orientation
+	}
+	return nil
+}
