2068f459624ea304ac729284cd1bc6b12f84a963
Kenny Levinsen <kl@kl.wtf>
2022-10-11 14:36:15 +0200

    Change arg to be command to run

--- main.go
+++ main.go
@@ -3,17 +3,14 @@ package main
 import (
 	"os"
 	"time"
+	"os/exec"
 
 	libui "git.sr.ht/~sircmpwn/aerc/lib/ui"
 )
 
+const proxyDisplay = "wlhax-0"
+
 func main() {
-	var proxyDisplay string
-	if len(os.Args) > 1 {
-		proxyDisplay = os.Args[1]
-	} else {
-		proxyDisplay = "wlhax-0"
-	}
 	remoteDisplay, ok := os.LookupEnv("WAYLAND_DISPLAY")
 	if !ok {
 		panic("No WAYLAND_DISPLAY set")
@@ -27,6 +24,11 @@ func main() {
 	defer proxy.Close()
 
 	dash := NewDashboard(proxy)
+
+	if len(os.Args) > 1 {
+		cmd := exec.Command(os.Args[1], os.Args[2:]...)
+		cmd.Start()
+	}
 
 	ui, err := libui.Initialize(dash)
 	if err != nil {
